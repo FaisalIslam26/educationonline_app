90ad5179204626f831d780461163322fd3528d38
FaisalIslam26 <faisalislam_nissan@live.com>
2022-05-16 19:31:47 +0600

    this is my second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:test1_app/screens/home.dart';
+import 'package:test1_app/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -42,86 +42,178 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFF176),
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              CarouselSlider(
-                items: slideImage
-                    .map((image) => Builder(builder: (BuildContext context) {
-                          return Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(40),
-                              image: DecorationImage(
-                                  image: AssetImage(image), fit: BoxFit.fill),
-                            ),
-                          );
-                        }))
-                    .toList(),
-                options: CarouselOptions(
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      activeIndex = index;
-                    });
-                  },
-                  height: 300,
-                  viewportFraction: 1,
-                  autoPlay: true,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: AnimatedSmoothIndicator(
-                    activeIndex: activeIndex, count: slideImage.length),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: Container(
-              height: 80,
-              width: 380,
-              child: GridView.builder(
-                itemCount: menuImage.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1 / 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                ),
-                itemBuilder: (context, index) => ClipRRect(
-                  // borderRadius: BorderRadius.circular(25),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+      backgroundColor: Colors.grey,
+      // Color(0xFFFFF176),
+
+      body: SingleChildScrollView(
+        child: Expanded(
+          child: Container(
+            decoration: BoxDecoration(color: Colors.transparent),
+            height: 900,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    CarouselSlider(
+                      items: slideImage
+                          .map((image) =>
+                              Builder(builder: (BuildContext context) {
+                                return Container(
+                                  height: 120,
+                                  width: 400,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    image: DecorationImage(
+                                        image: AssetImage(image),
+                                        fit: BoxFit.fill),
+                                  ),
+                                );
+                              }))
+                          .toList(),
+                      options: CarouselOptions(
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            activeIndex = index;
+                          });
+                        },
+                        // height: 300,
+                        viewportFraction: 1,
+                        autoPlay: true,
+                      ),
                     ),
-                    color: Color(0xFFFFFDE7),
-                    elevation: 10,
-                    child: GridTile(
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            menuImage[index],
-                            fit: BoxFit.fill,
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: AnimatedSmoothIndicator(
+                          activeIndex: activeIndex, count: slideImage.length),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 500,
+                      // width: 500,
+                      child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: menuImage.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 1.8,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 5,
+                        ),
+                        itemBuilder: (context, index) => Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
                           ),
-                          Text(
-                            title[index],
-                            style: TextStyle(fontSize: 18),
+                          // color: Color(0xFFFFFDE7),
+                          color: Colors.amber,
+                          elevation: 20,
+                          child: GridTile(
+                            child: Image.asset(
+                              menuImage[index],
+                              fit: BoxFit.contain,
+                              height: 40,
+                              scale: 5,
+                            ),
+                            footer: GridTileBar(
+                              leading: Text(
+                                title[index],
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      color: Color(0xFFFFF176),
+                      elevation: 20,
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.blue,
+                              child: Icon(Icons.person),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('ধন্যবাদ😍'),
+                              Text(
+                                'আমাদের কে সাপোর্ট করার জন্য  ও ভবিষ্যতে আপসটি \n আরো উন্নয়নের জন্য প্লে স্টোরে \n আমাদের কে রেটিং দিন',
+                                maxLines: 4,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
 }
+
+
+
+
+
+
+//  Column(
+//         children: [
+//           Stack(
+//             alignment: Alignment.bottomCenter,
+//             children: [
+//               CarouselSlider(
+//                 items: slideImage
+//                     .map((image) => Builder(builder: (BuildContext context) {
+//                           return Container(
+//                             height: 200,
+//                             width: 500,
+//                             decoration: BoxDecoration(
+//                               borderRadius: BorderRadius.circular(40),
+//                               image: DecorationImage(
+//                                   image: AssetImage(image), fit: BoxFit.fill),
+//                             ),
+//                           );
+//                         }))
+//                     .toList(),
+//                 options: CarouselOptions(
+//                   onPageChanged: (index, reason) {
+//                     setState(() {
+//                       activeIndex = index;
+//                     });
+//                   },
+//                   height: 300,
+//                   viewportFraction: 1,
+//                   autoPlay: true,
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(10.0),
+//                 child: AnimatedSmoothIndicator(
+//                     activeIndex: activeIndex, count: slideImage.length),
+//               ),
+//             ],
+//           ),
+//           
+//         ],
+//       ),
